24cc0c88a5206188271eec8a3575808cf1808a48
Olena Kostiuk <helena.kostyuk@gmail.com>
2020-01-29 11:40:11 +0200

    Добавьте файлы проекта.

---
+++ UnitClass/IAttack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitClass
+{
+    interface IAttack
+    {
+
+        void TakeDamage(int damage);
+        void Attack(Unit unit);
+
+    }
+}
---
+++ UnitClass/UnitTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitClass
+{
+    [TestFixture]
+    class UnitTest
+    {
+        [Test]
+        public void CreateUnit()
+        {
+            new Elf("Legolas", 50, 5);
+        }
+
+        [Test]
+        public void GetHealthTest()
+        {
+            Unit unit = new Elf("Legolas", 50, 5);
+            Assert.AreEqual(unit.GetHealth(), 50);
+        }
+
+        [Test]
+        public void TakeDamageTest()
+        {
+            Unit unit = new Elf("Legolas", 100, 20);
+            unit.TakeDamage(15);
+            Assert.AreEqual(85, unit.GetHealth());
+        }
+
+        [Test]
+        public void AttackTest()
+        {
+            Unit elf = new Elf();
+            Unit orc = new Orc();
+
+            elf.Attack(orc);
+            Assert.AreEqual(184, orc.GetHealth());
+
+        }
+    }
+}
